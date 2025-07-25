15275c7ce05cc83ecc45b43d788230ded6f00d22
Ashok-Bhatt <ashokbhatt2048@gmail.com>
2025-07-25 16:53:20 +0530

    Migrate web scrapping logic from selenium to browserless api

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from flask import Flask, jsonify
-import time
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -16,36 +14,33 @@ def index():
 @app.route("/search/<title>")
 def search(title):
 
-    driver = None
-    try:
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_experimental_option("detach", True)
+    TOKEN = os.getenv("BROWSERLESS_API_KEY")
+    url = f"https://production-sfo.browserless.io/scrape?token={TOKEN}"
+    headers = {
+        "Cache-Control": "no-cache",
+        "Content-Type": "application/json"
+    }
 
-        web_url = f"https://en.wikipedia.org/wiki/{title}"
+    data = {
+        "url": f"https://en.wikipedia.org/wiki/{title}",
+        "elements": [
+            { "selector": "p" }
+        ],
+        "waitForSelector": {
+            "selector": "h1",
+            "timeout": 1000
+        }
+    }
 
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-        driver.get(web_url)
+    api_response = requests.post(url, headers=headers, json=data)
+    result = api_response.json()
+    
+    response = []
 
-        time.sleep(1)
+    for index, data in enumerate(result["data"][0]["results"]):
+        response.append(data["text"])
 
-        paragraphs = driver.find_elements(By.TAG_NAME, value="p")
-        result = []
-
-        for paragraph in paragraphs:
-            result.append(paragraph.text)
-        
-        return jsonify(result)
-    except Exception as e:
-        return jsonify({
-            "message" : "Couldn't search",
-            "error" : str(e),
-        })
-    finally:
-        if (driver):
-            driver.close()
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
