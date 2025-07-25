7421d031ee2263075e05eb38c1e9a50747833499
Ashok-Bhatt <ashokbhatt2048@gmail.com>
2025-07-25 11:19:33 +0530

    Add changes to selenium web driver setup

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from flask import Flask, jsonify
@@ -24,7 +26,7 @@ def search(title):
 
         web_url = f"https://en.wikipedia.org/wiki/{title}"
 
-        driver = webdriver.Chrome(options=chrome_options)
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
         driver.get(web_url)
 
         time.sleep(1)
