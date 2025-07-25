b853dbd377553009e639dcb9557d9fbe9b86ce0c
Ashok-Bhatt <ashokbhatt2048@gmail.com>
2025-07-25 17:30:50 +0530

    Reimplement Error handling

--- app.py
+++ app.py
@@ -14,34 +14,37 @@ def index():
 @app.route("/search/<title>")
 def search(title):
 
-    TOKEN = os.getenv("BROWSERLESS_API_KEY")
-    url = f"https://production-sfo.browserless.io/scrape?token={TOKEN}"
-    headers = {
-        "Cache-Control": "no-cache",
-        "Content-Type": "application/json"
-    }
-
-    data = {
-        "url": f"https://en.wikipedia.org/wiki/{title}",
-        "elements": [
-            { "selector": "p" }
-        ],
-        "waitForSelector": {
-            "selector": "h1",
-            "timeout": 1000
+    try:
+        TOKEN = os.getenv("BROWSERLESS_API_KEY")
+        url = f"https://production-sfo.browserless.io/scrape?token={TOKEN}"
+        headers = {
+            "Cache-Control": "no-cache",
+            "Content-Type": "application/json"
         }
-    }
 
-    api_response = requests.post(url, headers=headers, json=data)
-    result = api_response.json()
-    
-    response = []
+        data = {
+            "url": f"https://en.wikipedia.org/wiki/{title}",
+            "elements": [
+                { "selector": "p" }
+            ],
+            "waitForSelector": {
+                "selector": "h1",
+                "timeout": 1000
+            }
+        }
 
-    for index, data in enumerate(result["data"][0]["results"]):
-        response.append(data["text"])
+        api_response = requests.post(url, headers=headers, json=data)
+        result = api_response.json()
+        
+        response = []
 
-    return response
+        for index, data in enumerate(result["data"][0]["results"]):
+            response.append(data["text"])
+
+        return jsonify({"response": response})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
